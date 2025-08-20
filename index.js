a5f76dc2a6cce8038712f4e25e1209929f2bd5ec
vamshi-krishna122 <vk7937190@gmail.com>
2025-08-20 11:28:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// This function runs when the button is clicked
+function sayHello() {
+    alert("Hello from JavaScript! 🚀");
+    console.log("Button clicked! This is logged in the console.");
+    console.log("Button clicked! This is logged in the console 222.");
+    console.log("Hello from feature-hello branch");
+}
+
+// Some example utility functions
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function sumArray(arr) {
+    return arr.reduce((sum, num) => sum + num, 0);
+}
+
+// Testing the functions in console
+console.log(capitalize("hello world")); // Hello world
+console.log(sumArray([1, 2, 3, 4])); // 10
+/*
+E:\MURALI\10Days-ReactJS\day2-training\sample-js-project>node script.js
+Hello world
+10
+*/
