072f86da08e777da95c60ed59c344ab059b5aa26
vamshi-krishna122 <vk7937190@gmail.com>
2025-08-21 10:32:14 +0530

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function sayHello() {
     console.log("Button clicked! This is logged in the console.");
     console.log("Button clicked! This is logged in the console 222.");
     console.log("Hello from feature-hello branch");
+    console.log("Hello Welcome to Vamshi");
 }
 
 // Some example utility functions
